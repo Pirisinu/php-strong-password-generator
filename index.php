550d1da17e96eda2f3a7a105e8cccc6d81f30ed5
Pirisinu <palazzialessandro2@gmail.com>
2023-10-27 15:46:39 +0200

    [ADD]Css Layout

--- index.php
+++ index.php
@@ -9,9 +9,24 @@ $saluto = "ciao da php";
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- CSS -->
+  <link rel="stylesheet" href="style.css">
   <title>Password Generator</title>
 </head>
 <body>
-  <h1><?php echo $saluto ?></h1>
+  <h1>Strong Password Generator</h1>
+  <h2>Genera una password sicura</h2>
+  <div class="container">
+    <div class="info">
+      <p>Scegliere una password con un minimo di 8 caratteri e un massimo di 32 caratteri</p>
+    </div>
+    <form class="setting">
+      <span>Lunghezza password:</span>
+      <input type="number"><br>
+
+      <button class="submit" type="submit">Invia</button>
+      <button class="reset" type="reset">Annulla</button>
+    </form>
+  </div>
 </body>
 </html>
